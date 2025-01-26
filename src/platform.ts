883c1739910afe78d76bc6cd21b6cd9883bfea2c
hovercraft <accounts@hovestadt.me>
2025-01-26 18:26:04 +0100

    Fixed crash when no device is configured

--- src/platform.ts
+++ src/platform.ts
@@ -58,41 +58,43 @@ export class DenonTelnetPlatform implements DynamicPlatformPlugin {
     let existingAccessoriesUuids = Array.from(this.accessories.keys());
 
     // loop over the configured devices and register each one if it has not already been registered
-    for (const deviceConfig of this.config.devices) {
+    if (this.config.devices) {
+      for (const deviceConfig of this.config.devices) {
 
-      // generate a unique id for the accessory this should be generated from something globally unique, 
-      // but constant, for example, the device serial number or MAC address
-      const uuid = this.api.hap.uuid.generate(deviceConfig.serialNumber);
+        // generate a unique id for the accessory this should be generated from something globally unique, 
+        // but constant, for example, the device serial number or MAC address
+        const uuid = this.api.hap.uuid.generate(deviceConfig.serialNumber);
 
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.get(uuid);
+        // see if an accessory with the same uuid has already been registered and restored from
+        // the cached devices we stored in the `configureAccessory` method above
+        const existingAccessory = this.accessories.get(uuid);
 
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+        if (existingAccessory) {
+          // the accessory already exists
+          this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
-        // create the accessory handler for the restored accessory
-        new DenonTelnetAccessory(this, existingAccessory, deviceConfig, this.log);
+          // create the accessory handler for the restored accessory
+          new DenonTelnetAccessory(this, existingAccessory, deviceConfig, this.log);
 
-        // after it has been restored, the existing accessory should be removed form the uuid list
-        existingAccessoriesUuids = existingAccessoriesUuids.filter(uuid => uuid !== existingAccessory.UUID);
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', deviceConfig.name);
+          // after it has been restored, the existing accessory should be removed form the uuid list
+          existingAccessoriesUuids = existingAccessoriesUuids.filter(uuid => uuid !== existingAccessory.UUID);
+        } else {
+          // the accessory does not yet exist, so we need to create it
+          this.log.info('Adding new accessory:', deviceConfig.name);
 
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(deviceConfig.name, uuid);
+          // create a new accessory
+          const accessory = new this.api.platformAccessory(deviceConfig.name, uuid);
 
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = deviceConfig;
+          // store a copy of the device object in the `accessory.context`
+          // the `context` property can be used to store any data about the accessory you may need
+          accessory.context.device = deviceConfig;
 
-        // create the accessory handler for the newly create accessory
-        new DenonTelnetAccessory(this, accessory, deviceConfig, this.log);
+          // create the accessory handler for the newly create accessory
+          new DenonTelnetAccessory(this, accessory, deviceConfig, this.log);
 
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+          // link the accessory to your platform
+          this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+        }
       }
     }
 
